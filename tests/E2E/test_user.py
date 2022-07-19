b07c4ec64b26aecabb53a0d52eafaff88918c06f
amar <barake.amar@gmail.com>
2022-07-19 14:23:35 +0300

    Final commit

--- tests/E2E/test_user.py
+++ tests/E2E/test_user.py
@@ -27,24 +27,26 @@ def driver():
     driver.close()
 
 
+
+
 # Positive Scenario
-# def test_Registration(driver):
-#     driver.get('http://localhost:8000/')
-#     driver.maximize_window()
-#     driver.find_element(By.CSS_SELECTOR, "#navbarScroll > div > a:nth-child(2)").click()
-#     driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > div > div > a").click()
-#     driver.find_element(By.CSS_SELECTOR, "#name").send_keys("amaroo")
-#     Actual = driver.find_element(By.CSS_SELECTOR, "#name").text
-#     driver.find_element(By.CSS_SELECTOR, "#email").send_keys("amar.abs3dsfsf5@gmail.com")
-#     driver.find_element(By.CSS_SELECTOR, "#password").send_keys("Amar1234&*")
-#     driver.find_element(By.CSS_SELECTOR, "#passwordConfirm").send_keys("Amar1234&*")
-#     driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > form > button").click()
-#     time.sleep(3)
-#     # driver.find_element(By.CSS_SELECTOR, "#username").click()
-#     # driver.find_element(By.LINK_TEXT, "Profile").click()
-#     # name = driver.find_element(By.CSS_SELECTOR, "#name")
-#     # Expected = name.text
-#     # assert Expected == Actual
+def test_Registration(driver):
+    driver.get('http://localhost:8000/')
+    driver.maximize_window()
+    driver.find_element(By.CSS_SELECTOR, "#navbarScroll > div > a:nth-child(2)").click()
+    driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > div > div > a").click()
+    driver.find_element(By.CSS_SELECTOR, "#name").send_keys("amaroo")
+    Actual = driver.find_element(By.CSS_SELECTOR, "#name").text
+    driver.find_element(By.CSS_SELECTOR, "#email").send_keys("amar.abs3dsfssdadsf5@gmail.com")
+    driver.find_element(By.CSS_SELECTOR, "#password").send_keys("Amar1234&*")
+    driver.find_element(By.CSS_SELECTOR, "#passwordConfirm").send_keys("Amar1234&*")
+    driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > form > button").click()
+    time.sleep(3)
+    driver.find_element(By.CSS_SELECTOR, "#username").click()
+    driver.find_element(By.LINK_TEXT, "Profile").click()
+    name = driver.find_element(By.CSS_SELECTOR, "#name")
+    Expected = name.text
+    assert Expected == Actual
 
 
 # def test_login(driver):
